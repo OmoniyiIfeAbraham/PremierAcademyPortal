9a599dc085ef03fd4b54791e237e56d36d30194e
Ife Abraham <ife04abraham@gmail.com>
2025-07-12 02:29:09 +0100

    Working on Student Dashboard Frontend

--- routes/admin/dashboard/Index.js
+++ routes/admin/dashboard/Index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const ProfileModel = require("../../../models/Teachers/Profile.model");
+const DetailModel = require("../../../models/Subjects/Detail.model");
+const ClassDetailModel = require("../../../models/Classes/ClassDetail.model");
+const StudentProfileModel = require("../../../models/Students/StudentProfile.model");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const sess = req.session;
   if (sess.email && sess.password && sess.identifier === "admin") {
-    res.render("admin/dashboard/index");
+    const teachers = await ProfileModel.find({});
+    const subjects = await DetailModel.find({});
+    const classes = await ClassDetailModel.find({});
+    const students = await StudentProfileModel.find({});
+    res.render("admin/dashboard/index", {
+      teachers,
+      subjects,
+      classes,
+      students,
+    });
   } else {
     res.redirect("/portal/admin/auth/login");
   }
